49c65431d87c246ec6074f18b63778c08dbade44
Dmitrii Kurlov <dmitriik@protonmail.com>
2022-03-05 22:56:27 +0700

    refactor: rename project

--- tests/test_hab_scraper.py
+++
@@ -1,5 +0,0 @@
-import hab_scraper
-
-
-def test_template_project_python():
-    assert hab_scraper.__version__ == "0.1.0"
---
+++ tests/test_hub_scraper.py
@@ -0,0 +1,5 @@
+import hub_scraper
+
+
+def test_template_project_python():
+    assert hub_scraper.__version__ == "0.1.0"
