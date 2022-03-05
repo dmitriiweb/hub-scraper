170c54202e33a91b23e4405c666bd44abf427689
Dmitrii Kurlov <dmitriik@protonmail.com>
2022-03-06 01:04:00 +0700

    feat: add more start parameters

--- hub_scraper/console/application.py
+++ hub_scraper/console/application.py
@@ -1,6 +1,8 @@
 import asyncio
-from pathlib import Path
+
 from functools import wraps
+from pathlib import Path
+from typing import Optional
 
 import click
 
@@ -58,7 +60,93 @@ def coro(f):
     default=False,
     is_flag=True,
 )
-async def main(url: str, output_folder: str, threads: int, time_delay: int, images: bool):
+@click.option(
+    "--no-sandbox",
+    help="If true will skip articles from the sandbox. Default False",
+    type=bool,
+    required=False,
+    default=False,
+    is_flag=True,
+)
+@click.option(
+    "-fr",
+    "--filter-rating",
+    help="Filter articles by rating, default: 0",
+    type=int,
+    required=False,
+    default=0,
+)
+@click.option(
+    "-fc",
+    "--filter-comments",
+    help="Filter articles by comments count, default: 0",
+    type=int,
+    required=False,
+    default=0,
+)
+@click.option(
+    "-fc",
+    "--filter-comments",
+    help="Filter articles by comments count, default: 0",
+    type=int,
+    required=False,
+    default=0,
+)
+@click.option(
+    "-fb",
+    "--filter-bookmarks",
+    help="Filter articles by bookmarks count, default: 0",
+    type=int,
+    required=False,
+    default=0,
+)
+@click.option(
+    "-fb",
+    "--filter-tags",
+    help='Filter articles by list of tags, e.g. "Django, tutorial". Default: None',
+    type=str,
+    required=False,
+    default=None,
+)
+@click.option(
+    "-fk",
+    "--filter-keywords",
+    help='Filter articles by list of key-words for title, e.g. "Django, tutorial". Default: None',
+    type=str,
+    required=False,
+    default=None,
+)
+@click.option(
+    "-fd",
+    "--filter-date",
+    help='Filter articles by date, e.g. "dd-mm-yyyy hh:mm". Default: None',
+    type=str,
+    required=False,
+    default=None,
+)
+@click.option(
+    "-fa",
+    "--filter-author",
+    help='Filter articles by list of authors, e.g. "he_boiko, OldTech". Default: None',
+    type=str,
+    required=False,
+    default=None,
+)
+async def main(
+    url: str,
+    output_folder: str,
+    threads: int,
+    time_delay: int,
+    images: bool,
+    no_sandbox: bool,
+    filter_comments: int,
+    filter_rating: int,
+    filter_bookmarks: int,
+    filter_tags: Optional[str],
+    filter_keywords: Optional[str],
+    filter_date: Optional[str],
+    filter_author: Optional[str],
+):
     output_folder = Path(output_folder)
 
 
